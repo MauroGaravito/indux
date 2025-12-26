55adb998756dfa34d64bda45b561e81cd16c3115
maurogaravito <m.garavito82@gmail.com>
2025-12-27 10:29:57 +1100

    chore: stop tracking build artifacts (dist folders)

--- api/dist/db.js
+++
@@ -1,6 +0,0 @@
-import mongoose from 'mongoose';
-export async function connectDB() {
-    const uri = process.env.MONGO_URI || 'mongodb://localhost:27017/indux';
-    await mongoose.connect(uri);
-    console.log('Connected to Mongo');
-}
